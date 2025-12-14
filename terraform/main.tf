20e23a8678dcc69e7db7d138bae84bd831804a94
wu <democritushygge@gmail.com>
2025-12-14 15:37:58 +0100

    Project structure with gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "${var.project_name}-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deply resources"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  default     = "cloud-native-app"
+}
