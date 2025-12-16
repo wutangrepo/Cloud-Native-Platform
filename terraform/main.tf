0573c659a02942a9489995982ab590f944171129
wu <democritushygge@gmail.com>
2025-12-16 15:36:42 +0100

    fix: enable force_delete on ECR to allow terraform destroy to clean up images

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,8 @@ resource "aws_ecr_repository" "app_repo" {
   name                 = "${var.project_name}-repo"
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
