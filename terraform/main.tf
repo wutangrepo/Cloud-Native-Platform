d76fb609e557e322b35fbf837696cae796f92cbf
wu <democritushygge@gmail.com>
2025-12-26 23:10:35 +0100

    fix a typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -153,7 +153,7 @@ resource "aws_iam_role" "eks_node_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sys:AssumeRole"
+        Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
