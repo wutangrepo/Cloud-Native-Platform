9391af956c265f368be50df75169a3006f38d98c
wu <democritushygge@gmail.com>
2025-12-28 23:32:32 +0100

    add EKS managed node group for worker nodes (EC2)

--- terraform/main.tf
+++ terraform/main.tf
@@ -183,7 +183,8 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 }
 
 # --- EKS cluster ---
-# Control plane components all are behind in AWS managed VPC, we just make a "contract" here to negotiate with AWS
+
+# Control plane components all are behind in AWS managed VPC, We just make a "contract" here to negotiate with AWS
 resource "aws_eks_cluster" "main" {
   name = var.project_name
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -198,8 +199,35 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
   }
-
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling, Otherwise, EKS will not be able to be properly deleted
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy,
+  ]
+}
+
+# --- EKS Node Group ---
+resource "aws_eks_node_group" "main" {
+  cluster_name = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-node-group"
+  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = 1
+    max_size = 2
+    min_size = 1
+  }
+
+  # We can save money by specifying instance_types = ["t3.small"] if deploying for a long time
+
+  # Keep here for future use as now we only have 1 worker node
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only,
+    aws_iam_role_policy_attachment.eks_cni_policy,
   ]
 }
