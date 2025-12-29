ab1fbef2d618383e9c47fe2ea457d6e82cd5718b
wu <democritushygge@gmail.com>
2025-12-29 22:36:21 +0100

    add outputs to configure kubectl

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "repository_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "configure_kubectl" {
+  value = "aws eks --region ${var.aws_region} --name ${aws_eks_cluster.main.name}"
+}
