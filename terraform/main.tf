0f9c36b88c07720b1110f48124e1dba32a19f0de
Wu Tang <164004724+wutangrepo@users.noreply.github.com>
2025-12-30 19:12:19 +0100

    fix(eks): correct subnet tags and disable private endpoint access (#16)
    - Updated subnet tags to match the full cluster name ('-cluster') so Load Balancers can auto-discover subnets.
    
    - Set endpoint_private_access to false to ensure nodes connect via the public endpoint.
    
    - Added comments documenting the SSM AccessDeniedException error.
    
    -Remove an unnecessary owner tag
    
    -Add TO DO for enabling endpoint_private access in the future

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "aws_subnet" "public" {
   tags = {
     Name                                        = "${var.project_name}-public-subnet-${count.index + 1}"
     "kubernetes.io/role/elb"                    = "1"      # enable elb in public subnet
-    "kubernetes.io/cluster/${var.project_name}" = "shared" # Ownership, telling k8s cluster this subnet is available. Can safely omit only if AWS Load Balancer is configured well.
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared" # Ownership, telling k8s cluster this subnet is available. Can safely omit only if AWS Load Balancer is configured well.
   }
 }
 
@@ -54,7 +54,7 @@ resource "aws_subnet" "private" {
   tags = {
     Name                                        = "${var.project_name}-private-subnet-${count.index + 1}"
     "kubernetes.io/role/internal-elb"           = "1"
-    "kubernetes.io/cluster/${var.project_name}" = "shared"
+    "kubernetes.io/cluster/${var.project_name}-cluster" = "shared"
   }
 }
 
@@ -199,7 +199,11 @@ resource "aws_eks_cluster" "main" {
     subnet_ids = concat(aws_subnet.private[*].id, aws_subnet.public[*].id)
 
     endpoint_public_access  = true
-    endpoint_private_access = true # use Split-Horizon DNS as we have dns_hostnames and dns_support enabled, default is false to use NAT to visit on internet
+
+    # When set to true, it shows: SSM Agent unable to acquire credentials: <error>no valid credentials could be retrieved for ec2 identity.
+    # Default Host Management Err: error calling RequestManagedInstanceRoleToken: AccessDeniedException:
+    # Systems Manager's instance management role is not configured for account: XXXXXX
+    endpoint_private_access = false # TO DO: change to true for better security after figuring out SSM Agent issue
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling, Otherwise, EKS will not be able to be properly deleted
@@ -208,7 +212,6 @@ resource "aws_eks_cluster" "main" {
   ]
   tags = {
     Project = var.project_name
-    Owner   = "Wu"
   }
 }
 
@@ -242,6 +245,5 @@ resource "aws_eks_node_group" "main" {
 
   tags = {
     Project = var.project_name
-    Owner   = "Wu"
   }
 }
