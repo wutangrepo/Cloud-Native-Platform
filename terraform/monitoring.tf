7c037122fcd808fb875a70904f1434a6168692b4
Wu Tang <164004724+wutangrepo@users.noreply.github.com>
2026-01-08 01:37:23 +0100

    feat(terraform): configure helm provider and add kube-prometheus-stack (#22)
    - Added hashicorp/helm to required_providers and configured EKS exec plugin authentication.
    
    - Updated version constraints and ran terraform init -upgrade to lock new provider versions.
    
    - Added helm_release for Prometheus and Grafana monitoring (chart version ~> 80.13).
    
    - Disabled Grafana and Prometheus persistence to minimize EBS costs and prevent future default changes.
    
    - Added technical comments clarifying certificate_authority logic and EKS Node Group dependencies.

---
+++ terraform/monitoring.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "~> 80.13"
+  namespace  = "monitoring"
+
+  create_namespace = true
+
+  wait = true
+
+  set = [
+    {
+      name  = "grafana.persistence.enabled"
+      value = "false"
+    },
+    {
+      name  = "prometheus.prometheusSpec.storageSpec"
+      value = "" # # The default values is already for "" after check this chart's values.yaml for version 80.13 on Artifact Hub but keep it to avoid future updates changing the default value
+    }
+  ]
+
+  depends_on = [
+    aws_eks_node_group.main
+  ]
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,14 +1,33 @@
+# Providers
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.27"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1"
     }
   }
 
-  required_version = ">= 1.0"
+  required_version = ">= 1.14"
 }
 
+# Configuration options for providers
 provider "aws" {
   region = var.aws_region
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data) # certificate_authority attribute is a list of object usually containing only one object
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
+    }
+  }
 }
